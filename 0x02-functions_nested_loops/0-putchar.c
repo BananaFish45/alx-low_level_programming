4c46fd5de0af1da8bb2dd4b0929fa9ac6b17d28b
BananaFish <u18108378@tuks.co.za>
2022-09-13 10:27:11 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 
 int main ()
 {
 
-_putchar(_);
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
-_putchar(n);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 
 return 0;
 
 
-}
