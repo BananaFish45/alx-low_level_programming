1fa1ee0e9fc675b5e1bff45d473ed88318eb1b9c
BananaFish <62051532+BananaFish45@users.noreply.github.com>
2022-10-14 04:48:14 +0200

    Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * sum_them_all - A function that returns the sum of all its parameters
@@ -11,17 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list ap;
-    unsigned int i, nSum;
+    va_list args;
+    unsigned int i = 0, nSum = 0;
 
-    if (n == 0)
-        return (0);
+    if (n != 0)
+    {
+        va_start(args, n);
+        while (i < n)
+        {
+            nSum += va_arg(args, int);
+            i++;
+        }
+        va_arg(args);
+        return (nSum);
+    }
 
-    va_start(ap, n);
-
-    nSum = 0;
-    for (i = 0; i < n; i++)
-        nSum += va_arg(ap, unsigned int);
-    va_end(ap);
-    return (nSum);
+    return (0);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-#include <stdio.h>
 
-void printchar(va_list charlist);
-void printinteger(va_list charlist);
-void printfloat(va_list charlist);
-void printstring(va_list charlist);
-
-typedef struct characters
+typedef struct printer
 {
-    char *arguments;
+    char *symbol;
+    void (*print)(va_list arg);
+} printer_t;
 
-    void (*ptrfunc)(va_list);
-} characters;
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
